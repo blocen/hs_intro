9e952b76a2dc6741154dd4cc039f6d1e2d1bc0de
Raphael <44736846+tr4phix@users.noreply.github.com>
2021-07-26 11:01:31 +0200

    Higher order functions

--- Higher_order_functions.hs
+++ Higher_order_functions.hs
@@ -1,12 +1,20 @@
 
 module Higher_order_functions where
 
+applyTo1 :: (Int -> Int) -> Int
+applyTo1 func = func 1
+
 addThree :: Int -> Int
 addThree x = x + 3
+-- applyTo1 addThree
+
+doTwice :: (a -> a) -> a -> a 
+doTwice f x = f (f x)
+-- doTwice addThree 1
+
 
 -- map :: (a -> b) -> [a] -> [b]
 -- map addThree [1,2,3]
-
 
 -- a predicate that checks if a string is a palindrome
 palindrome :: String -> Bool
@@ -19,4 +27,6 @@ palindromes n = filter palindrome (map show [1..n])
 countAWords :: String -> Int
 countAWords string = length (filter startsWithA (words string))
   where startsWithA s = head s == 'a'
+
+  
   
